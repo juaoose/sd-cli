f2ee4396ce7d96135dae0583572058e58ff10d70
Juan <994594+juaoose@users.noreply.github.com>
2024-01-02 18:06:52 -0500

    nothing really of use

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='diffusion',
+    name='diff',
     version='0.1.0',
-    py_modules=['main'],
+    py_modules=['diff'],
     install_requires=[
         'Click',
     ],
     entry_points={
         'console_scripts': [
-            'main = main:hello',
+            'diff = diff:hello',
         ],
     },
 )
