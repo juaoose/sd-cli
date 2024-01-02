0e53da530299a94358f799d0532819f9074a2cef
Juan <994594+juaoose@users.noreply.github.com>
2024-01-02 18:07:06 -0500

    add diffusers

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ import click
 def hello(sentence):
   click.echo(f"Your sentence is {sentence}")
 
+  from diffusers import DiffusionPipeline
+
+  pipeline = DiffusionPipeline.from_pretrained("OFA-Sys/small-stable-diffusion-v0")
+  image = pipeline(sentence).images[0]
+
 if __name__ == '__main__':
   hello()
